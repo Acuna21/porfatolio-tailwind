af327f49db5607f4ad92ff2cc3bdfb665420cd7c
Sara Acuña <acunabenavidessaraelis@gmail.com>
2025-03-05 10:59:42 -0500

    review with js

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -33,3 +33,31 @@ export const projects=[
 
     }
 ]
+
+
+export const review = [
+    {
+        name: "Yulissa Anaya",
+        image:{
+            alt: "Imagen de perfil de Yulissa",
+            img: "https://www.clarin.com/img/2024/07/04/uteodLeuh_600x600__1.jpg"
+        },
+        review: "No solo es una excelente desarrolladora frontend, sino que también tiene una visión integral del software. Su capacidad para comprender arquitectura, seguridad y backend la convierte en una ingeniera completa"
+    },
+    {
+        name: "Mariano de la Barrera",
+        image:{
+            alt: "Imagen de perfil de Mariano",
+            img: "https://img.freepik.com/foto-gratis/concepto-libertad-senderista-montana_23-2148107064.jpg"
+        },
+        review: "La atención al detalle de Sara es impresionante. Cada línea de código que escribe está optimizada para rendimiento y escalabilidad, lo que la hace destacar en cualquier equipo de desarrollo"
+    },
+    {
+        name: "Jesus Chamorro",
+        image:{
+            alt: "Imagen de perfil de Jesus",
+            img: "https://plus.unsplash.com/premium_photo-1689568126014-06fea9d5d341?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGZvdG8lMjBkZSUyMHBlcmZpbHxlbnwwfHwwfHx8MA%3D%3D"
+        },
+        review: "Su pasión por la tecnología y el aprendizaje continuo es evidente en cada proyecto que desarrolla. Sara no solo programa, sino que innova"
+    }
+]
--- src/scripts/template.js
+++ src/scripts/template.js
@@ -1,7 +1,6 @@
-import { projects } from "./data.js"; 
+import { projects, review } from "./data.js"; 
 
 const templateProjects = document.querySelector('[data-template="project-card"]');
-
 const projectsUl = document.querySelector('#projects-list');
 
 projects.forEach((project) => {
@@ -10,9 +9,7 @@ projects.forEach((project) => {
     const image = card.querySelector('img');
     const title = card.querySelector('h4');
     const description = card.querySelectorAll('p')[0]; // Primer párrafo
-
     const tech = card.querySelector('.tech-list'); // Clase específica
-
     const link = card.querySelector('a');
 
     image.src = project.image.src;
@@ -24,7 +21,24 @@ projects.forEach((project) => {
 
     link.href = project.link;
 
-    console.log("aca")
-
     projectsUl.appendChild(card);
 });
+
+
+const templateReview = document.querySelector('[data-template="review-card"]');
+const reviewUl = document.querySelector('#review-list');
+
+review.forEach((review) => {
+    const reviewCard = templateReview.content.cloneNode(true);
+
+    const image = reviewCard.querySelector('img');
+    const name = reviewCard.querySelector('p');
+    const reviewText = reviewCard.querySelector('blockquote');
+
+    image.src = review.image.img;
+    image.alt = review.image.alt;
+    name.textContent = review.name;
+    reviewText.textContent = review.review;
+
+    reviewUl.appendChild(reviewCard);
+})
